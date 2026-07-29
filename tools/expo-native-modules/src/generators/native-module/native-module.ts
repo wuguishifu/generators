706dc6a1612470e3a29903e804249c4a3a8582c4
Bo Bramer <bramer.bo@gmail.com>
2026-07-28 22:00:14 -0700

    babel

--- tools/expo-native-modules/src/generators/native-module/native-module.ts
+++ tools/expo-native-modules/src/generators/native-module/native-module.ts
@@ -1,7 +1,8 @@
-import { formatFiles, type Tree } from '@nx/devkit';
+import { formatFiles, generateFiles, type Tree } from '@nx/devkit';
 import * as path from 'path';
 import type { NativeModuleGeneratorSchema } from './schema';
 import { addProjectDependencies } from './utils/add-project-deps';
+import { initRootBabelConfig } from './utils/init-root-babel-config';
 
 export default async function nativeModuleGenerator(
   tree: Tree,
@@ -12,6 +13,10 @@ export default async function nativeModuleGenerator(
   console.log({ projectRoot, name });
 
   addProjectDependencies(tree, options);
+
+  initRootBabelConfig(tree);
+
+  generateFiles(tree, path.join(__dirname, 'files/src'), projectRoot, {});
 
   await formatFiles(tree);
 }
