0fa291ad14b8594708a8b6df7c5cd6ab1c5e67de
Bo Bramer <bramer.bo@gmail.com>
2026-07-28 20:21:20 -0700

    scaffold generator

---
+++ tools/expo-native-modules/src/generators/native-module/native-module.ts
@@ -0,0 +1,25 @@
+import {
+  addProjectConfiguration,
+  formatFiles,
+  generateFiles,
+  type Tree,
+} from '@nx/devkit';
+import * as path from 'path';
+import type { NativeModuleGeneratorSchema } from './schema';
+
+export async function nativeModuleGenerator(
+  tree: Tree,
+  options: NativeModuleGeneratorSchema,
+) {
+  const projectRoot = `libs/${options.name}`;
+  addProjectConfiguration(tree, options.name, {
+    root: projectRoot,
+    projectType: 'library',
+    sourceRoot: `${projectRoot}/src`,
+    targets: {},
+  });
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+  await formatFiles(tree);
+}
+
+export default nativeModuleGenerator;
---
+++ tools/expo-native-modules/src/generators/native-module/schema.d.ts
@@ -0,0 +1,3 @@
+export interface NativeModuleGeneratorSchema {
+  name: string;
+}
