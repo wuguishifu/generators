cab4919a908055d2384b0094fd3b09540cb061dc
Bo Bramer <bramer.bo@gmail.com>
2026-07-28 20:17:31 -0700

    scaffold tool;

---
+++ tools/expo-native-modules/README.md
@@ -0,0 +1,7 @@
+# nx-expo-native-modules
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build nx-expo-native-modules` to build the library.
---
+++ tools/expo-native-modules/eslint.config.mjs
@@ -0,0 +1,31 @@
+import baseConfig from '../../eslint.config.mjs';
+
+export default [
+  ...baseConfig,
+  {
+    files: ['**/*.json'],
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}'],
+        },
+      ],
+    },
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
+  },
+  {
+    ignores: ['**/out-tsc'],
+  },
+  {
+    files: ['**/package.json'],
+    rules: {
+      '@nx/nx-plugin-checks': 'error',
+    },
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
+  },
+];
